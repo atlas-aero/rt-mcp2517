8ea566c01cacf0ea4cce47ca8e15219a44406842
moritzNeo <moritz.gellermann@neomium.eu>
2026-07-14 10:32:09 +0200

    more linting

--- src/mocks.rs
+++ src/mocks.rs
@@ -29,7 +29,7 @@ impl Clock for TestClock {
     const SCALING_FACTOR: Fraction = Fraction::new(1, 1_000_000);
 
     fn try_now(&self) -> Result<Instant<Self>, ClockError> {
-        if self.next_instants.borrow().len() == 0 {
+        if self.next_instants.borrow().is_empty() {
             return Err(ClockError::Unspecified);
         }
 
